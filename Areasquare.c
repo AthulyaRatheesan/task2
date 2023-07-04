9bbdf37ac22fc892d1ae36108c8738d05b289e35
athulya <athulya317@gmail.com>
2023-07-04 22:26:47 +0530

    commit

---
+++ Areasquare.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+   int side, aos;
+
+   printf("\nEnter the Length of Side : ");
+   scanf("%d", &side);
+
+   aos = side * side;
+   printf("Area of Square :%d",aos);
+
+}
